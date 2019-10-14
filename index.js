fa1dc9ecf5f12889e81a635f6e3831208f097de3
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-14 20:35:26 -0300

    Ambiente e conceitos

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const server = express();
 
+// query params = ?teste=1
+// route params = /users/1
+// requeste body = {"name": "Patrick", "age": 30}
+
 server.get('/teste', (req, res) => {
-  return res.json({message: 'Jesus é Rei'});
+  return res.json({ message: 'Jesus é Rei'});
+});
+
+server.get('/params', (req, res) => {
+  const { nome } = req.query;
+  return res.json({ message: `Hello ${nome}`});
+});
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  return res.json({ message: `ID do usuário: ${id}`});
 });
 
 server.listen(3000);
