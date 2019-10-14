b3b3ffc729437f132cd668f9f8f78c06c8285bcb
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-14 20:52:52 -0300

    CRUD

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
 
 // query params = ?teste=1
 // route params = /users/1
@@ -15,10 +16,38 @@ server.get('/params', (req, res) => {
   return res.json({ message: `Hello ${nome}`});
 });
 
+// CRUD - create, read, update, delete
 const users = ['Diego', 'Patrick', 'Victor'];
-server.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-  return res.json({ user: users[id]});
+
+server.get('/users', (req, res) => {
+  return res.json(users);
 });
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+  return res.json({ user: users[index]});
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+
+  return res.send();
+})
 
 server.listen(3000);
